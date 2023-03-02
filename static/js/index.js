4e675c6cdfc5a9e5f1c9acfd9cbaf1223f030ea7
Ismail Houhou <ismailhouhou.pro@gmail.com>
2023-03-02 15:49:52 +0100

    In home page: works section, added pop up video if a tag is clicked, started to add on hover effect to show thumbnail but still isn't finished

--- static/js/index.js
+++ static/js/index.js
@@ -23,6 +23,60 @@ function closeReel() {
     body[0].style.overflowY = 'auto';
 }
 
+// WORKS
+// works popup
+function showWork(id) {
+    work = document.getElementById('work-'+id);
+    work.style.display = 'block';
+    work.style.visibility = 'visible';
+    work.style.opacity = '1';
+
+    body = document.getElementsByTagName('body');
+    body[0].style.overflowY = 'hidden';
+}
+
+function closeWork(id) {
+    work = document.getElementById('work-'+id);
+    work.style.visibility = 'hidden';
+    work.style.opacity = '0';
+
+    video = document.getElementById('video-'+id);
+    video.pause();
+    video.currentTime = 0;
+    video.load();
+
+    body = document.getElementsByTagName('body');
+    body[0].style.overflowY = 'auto';
+}
+
+// works onhover
+
+
+function showThumbnail(id) {
+    thumbnail_image = document.getElementById('thumbnail-'+id);
+    thumbnail_name = document.getElementById('title-'+id);
+    works = document.getElementsByClassName('works');
+
+    thumbnail_name.addEventListener("mouseover", () => {
+        thumbnail_image.style.display = "block";
+        works[0].style.position = "relative";
+      });
+      
+      thumbnail_name.addEventListener("mouseleave", () => {
+        thumbnail_image.style.display = "";
+    });
+
+    window.addEventListener("mousemove", (e) => {
+        let x = e.offsetX,
+          y = e.offsetY;
+      
+        if (e.target == thumbnail_name) {
+            thumbnail_image.style.left = `${x}px`;
+            thumbnail_image.style.top = `${y}px`;
+        } 
+    });
+}
+
 // SERVICES CAROUSEL
 let serviceIndex = 1;
 showService(serviceIndex);
